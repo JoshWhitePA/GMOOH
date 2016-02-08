130fe4affa648b77353ca0855eac1a5b3b80b05a
JoshWhitePA <josh.white.pa@gmail.com>
2016-02-07 23:08:12 -0500

    Chose a php class for db and created class to seperate UI from Logic

--- PHPClasses/Checksheet.php
+++ PHPClasses/Checksheet.php
@@ -1,26 +1,98 @@
 <?php
-require('ChkDisp.php');
-
-$testID = "CSC355";
-$testDesc = "SoftEngine";
-$testGrade = "A";
-$creditHour = "3";
-
-
+	require_once("chkDisp.class.php");
+	$chk = new Chksheet();
  ?>
-
-
-
- <!DOCTYPE html>
+ 
+<!DOCTYPE html>
 <html>
 <head>
+<style>
+table {
+    border-collapse: collapse;
+    width: 100%;
+}
 
+th, td {
+    text-align: left;
+    padding: 8px;
+}
+
+tr:nth-child(even){background-color: #f2f2f2}
+
+th {
+    background-color: #4CAF50;
+    color: white;
+}
+</style>
 </head>
-<body>
-<table>
-  <?php
-    echo function displayRow($testID,$testDesc,$testGrade);
-   ?>
-</table>
-</body>
+</head>
+	<body>
+		<table>
+			  <tr>
+				<th>Oral Communication: 
+					<?php 
+						$chk->displaySingleRecs(1,1);
+					?>
+				</th>
+				<th></th> 
+				<th></th>
+				<th></th>
+			  </tr>
+			  <tr>
+				<td></td>
+				<td></td> 
+				<td></td>
+				<td></td>
+			  </tr>
+			  <tr>
+			  
+			  <th>Written Communication: 
+					<?php 
+						$chk->displaySingleRecs(1,2);
+					?>
+				</th>
+				<th></th> 
+				<th></th>
+				<th></th>
+			  </tr>
+			  <tr>
+				<td></td>
+				<td></td> 
+				<td></td>
+				<td></td>
+			  </tr>
+			  
+			  <th>Mathematics: 
+					<?php 
+						$chk->displaySingleRecs(1,3);
+					?>
+				</th>
+				<th></th> 
+				<th></th>
+				<th></th>
+			  </tr>
+			  <tr>
+				<td></td>
+				<td></td> 
+				<td></td>
+				<td></td>
+			  </tr>
+	
+			 <th>Wellness: 
+					<?php 
+						$chk->displaySingleRecs(1,4);
+					?>
+				</th>
+				<th></th> 
+				<th></th>
+				<th></th>
+			  </tr>
+			  <tr>
+				<td></td>
+				<td></td> 
+				<td></td>
+				<td></td>
+			  </tr>
+		</table>
+	</body>
 </html>
