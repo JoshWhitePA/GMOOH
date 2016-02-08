96528990b58c38b872ec5eac2e7ba8b147f7a5eb
jwhit159 <jwhit159@csitrd.(none)>
2016-02-08 10:05:21 -0500

    changes

--- PHPClasses/Checksheet.php
+++ PHPClasses/Checksheet.php
@@ -1,33 +1,6 @@
 <?php
 	require_once("chkDisp.class.php");
-	require_once 'meekrodb.2.3.class.php';
-	
-	DB::$user = 'jwhit159';
-	DB::$password = 'W@ckyRace5';
-	DB::$dbName = 'jwhit159_bookstore';
-	
- 	$chk = new Chksheet();
-		
-	$Dept = array();
-	$ClassNo = array();
-	$LowRange = array();
-	$HighRange = array();
-	$retrived = false;
-	
-	$select = "select Dept, ClassNo, LowRange, HighRange 
-				from PID p
-				INNER JOIN Restrictions r
-				ON p.CompID = r.CompID";
-	
-	$results = DB::query($select);
-	foreach ($results as $row) {
-  		 	array_push($Dept,$row["Dept"]);
-			array_push($ClassNo, $row["ClassNo"]);
-			array_push($LowRange,$row["LowRange"]);
-			array_push($HighRange,$row["HighRange"]);
-			$retrived = true;
-	}
- 	 
+	$chk = new Chksheet();
  ?>
  
 <!DOCTYPE html>
@@ -58,9 +31,7 @@ th {
 			  <tr>
 				<th>Oral Communication: 
 					<?php 
-						if($retrived){
-							$chk -> displayGenReqs(0, $Dept, $ClassNo, $LowRange, $HighRange);
-						}
+						$chk->displaySingleRecs(1,1);
 					?>
 				</th>
 				<th></th> 
@@ -73,9 +44,55 @@ th {
 				<td></td>
 				<td></td>
 			  </tr>
-			<?php
-
-			?>
+			  <tr>
+			  
+			  <th>Written Communication: 
+					<?php 
+						$chk->displaySingleRecs(1,2);
+					?>
+				</th>
+				<th></th> 
+				<th></th>
+				<th></th>
+			  </tr>
+			  <tr>
+				<td></td>
+				<td></td> 
+				<td></td>
+				<td></td>
+			  </tr>
+			  
+			  <th>Mathematics: 
+					<?php 
+						$chk->displaySingleRecs(1,3);
+					?>
+				</th>
+				<th></th> 
+				<th></th>
+				<th></th>
+			  </tr>
+			  <tr>
+				<td></td>
+				<td></td> 
+				<td></td>
+				<td></td>
+			  </tr>
+	
+			 <th>Wellness: 
+					<?php 
+						$chk->displaySingleRecs(1,4);
+					?>
+				</th>
+				<th></th> 
+				<th></th>
+				<th></th>
+			  </tr>
+			  <tr>
+				<td></td>
+				<td></td> 
+				<td></td>
+				<td></td>
+			  </tr>
 		</table>
 	</body>
 </html>
--- PHPClasses/chkDisp.class.php
+++ PHPClasses/chkDisp.class.php
@@ -1,23 +1,48 @@
 <?php
-class Chksheet
-  {
+class Chksheet {
+  	
+  	function __construct() {
+  		require_once 'meekrodb.2.3.class.php';
+       	DB::$user = 'jwhit159';
+		DB::$password = 'W@ckyRace5';
+		DB::$dbName = 'jwhit159_bookstore';
+   }
+   
     public function displayRow($classID, $classDesc, $classGrade){
       echo '<tr><td>'. $classID .'</td><td>' . $classDesc . '</td><td>' . $classGrade . '</td></tr>';
     }
 	
-	 function displayGenReqs($idx, $Dept, $ClassNo, $LowRange, $HighRange) {
-				echo $Dept[$idx] . " "; 
-				if(!is_null($ClassNo[$idx])){
-					echo $ClassNo[$idx] . " ";
-				} elseif(!is_null($HighRange[$idx])){
-					echo $LowRange[$idx] . " - " . $HighRange[$idx];
-				} else{
-					echo $LowRange[$idx] . " or Above.";
+      
+      function displaySingleRecs($SecID,$PosID){
+		$select = "select Dept, ClassNo, LowRange, HighRange 
+					from PID p
+					INNER JOIN Restrictions r
+					ON p.CompID = r.CompID
+					where SecID = '". $SecID ."' and PosID = '" . $PosID ."' ";
+	
+		$results = DB::query($select);
+		foreach ($results as $row) {
+			$Dept = $row["Dept"];
+			$ClassNo = $row["ClassNo"];
+			$LowRange = $row["LowRange"];
+			$HighRange = $row["HighRange"];
+			if(!is_null($HighRange) || !is_null($ClassNo) || $LowRange != 0){
+				echo $Dept . ": "; 
+			}
+				if(!is_null($ClassNo)){
+					echo $ClassNo . " ";
+				} elseif(!is_null($HighRange)){
+					echo $LowRange . " - " . $HighRange;
 				}
-    } 
+				 elseif(is_null($HighRange) && is_null($ClassNo) && $LowRange == 0){
+					echo "Any $Dept Course";
+				}
+				else{
+					echo $LowRange . " or Above.";
+				}
+		}
+      }
+     
 
-  }
-
-
-
+}
 ?>
