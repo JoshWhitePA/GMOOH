fecc678770422af7a80ef9712494973de941733c
ccarr419 <ccarr419@live.kutztown.edu>
2016-02-25 00:24:10 -0500

    Cleaned up code for login pages
    1.) I put stray css in the php file into the actual css file.
    2.) I got rid of the .special class, and put the attributes under the th
    tag
    a.) this fixed an unknown issue with some css not displaying properly
    3.) I used better sizing techniques for the div classes
    4.) I fixed issue with buttons not directing to the correct location
    (the destination's file extension was html and not php)

--- Interface/login.php
+++ Interface/login.php
@@ -17,40 +17,38 @@
 	<link rel = "stylesheet" type = "text/css" href = "Styles/gmoohStyle.css"/>
 	</head>
 	<body>
-		<div class = "one">
-			<div class = "special">			
-				<form action="login.php" method="post" >
-					<table align = "center" width = "450px">
-						<tr>
-							<td rowspan = "6" align = "left" style = "vertical-align: middle">
-								<img src = "Images/kutztownLogo.png" width = "100px"/>
-							</td>
-							<td rowspan = "10" width = "50px"/>
-							<th align="right">Email &nbsp;</th>
-							<td align="right"><input type="email" required/></td>
-						</tr>
-						<tr>
-							<th align = "right">Password &nbsp;</th>
-							<td align = "right"><input type="password" required/></td>
-						</tr>
-						<tr height = "10px"></tr>
-						<tr>
-							<td colspan = "2" align = "center">
+		<div class = "one">	
+			<form action="login.php" method="post" >
+				<table>
+					<tr>
+						<td rowspan = "6" align = "left" style = "vertical-align: middle">
+							<img src = "Images/kutztownLogo.png" width = "100px"/>
+						</td>
+						<td rowspan = "10" width = "50px"></td>
+						<th>Email &nbsp;</th>
+						<td><input type="email" required/></td>
+					</tr>
+					<tr>
+						<th>Password &nbsp;</th>
+						<td><input type="password" required/></td>
+					</tr>
+					<tr height = "10px"></tr>
+					<tr>
+						<th colspan = "2" align = "center">
 							<button class="button1" type="submit" value="Submit">Submit</button>
 							<button class="button1" type="reset" value="Reset">Clear</button>
-							</td>
-						</tr>		
-						<tr height = "10px"></tr>
-						<tr>
-							<td colspan = "2" align = "center">
-								<input type = "button" class = "button1" 
-									onclick = "location.href='createAccount.html'" 
-									value = "Create New Account"/>
-							</td>
-						</tr>
-					</table>
-				</form>
-			</div>
+						</th>
+					</tr>		
+					<tr height = "10px"></tr>
+					<tr>
+						<th colspan = "2" align = "center">
+							<input type = "button" class = "button1" 
+								onclick = "location.href='createAccount.php'" 
+								value = "Create New Account"/>
+						</th>
+					</tr>
+				</table>
+			</form>
 		</div>
 	</body>
 </html>
