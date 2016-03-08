6f695d0857f03943d124bd5c2fd3a9f063341ab5
ccarr419 <ccarr419@live.kutztown.edu>
2016-03-07 23:13:08 -0500

    Added Scripts Fold To Interface
    1.) Added Script Folder
    a.) jQuery JavaScript library (normal and min version)
    b.) fusionCharts JavaScript library for progress charts
    
    2.) Changed all script src's in website pages to jQuery min file new
    script folder

--- Interface/gmoohHome.php
+++ Interface/gmoohHome.php
@@ -3,7 +3,7 @@
 	<head>
 		<title>Your Official Checksheet</title>
 		<link rel = "stylesheet" type = "text/css" href = "Styles/gmoohMasterStyle.css"/>	
-		<script src = "Tests/Scripts/jquery-1.12.0.js"></script>
+		<script src = "Scripts/jquery-1.12.0.min.js"></script>
 		<script>
 			$(document).ready(function(){
 				$("#master").load("MasterPages/masterPage.html");
