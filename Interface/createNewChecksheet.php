e2b366c178ccab952929887f3148e7f2454d8f03
ccarr419 <ccarr419@live.kutztown.edu>
2016-03-14 14:00:43 -0400

    Hopefully Re-updated Everything Correctly
    This should be everything I uploaded over the past week that some how
    wasn't committed to the master even it said it was on my end. The only
    work to be done to the prototype checksheet page is to create a new css
    file for the checksheet to properly display in-page.

--- Interface/createNewChecksheet.php
+++ Interface/createNewChecksheet.php
@@ -6,7 +6,60 @@
 		<script src = "Scripts/jquery-1.12.0.min.js"></script>
 		<script>
 			$(document).ready(function(){
-				$("#master").load("MasterPages/masterPage.html");
+				$("#master").load("MasterPages/masterPage.html", function() {
+					$("#left").css("visibility", "visible");
+					$("#right").css("visibility", "visible");
+					$("#mainSection")
+						.append("<select name = 'currentChecksheet'>"
+								+ "<option value = 'sd' selected = 'selected'>CSC SD</option>"
+								+ "<option value = 'Msd'>CSC SD Master</option>"
+								+ "<option value = 'msd'>CSC SD Minor</option>"
+								+ "<option value = 'it'>CSC IT</option>"
+								+ "<option value = 'Mit'>CSC IT Master</option>"
+								+ "<option value = 'mit'>CSC IT Minor</option>"
+								+ "</select>"
+								+ "<div id = 'innerSection' class = 'innerSection'></div>");
+					$("#left")
+						.append("Courses<br/><br/><div id = 'leftInnerSection' class = 'leftInnerSection' style = 'color: green'>Prerequesites Met</div>"
+							+ "<div class = 'newSection'><br/></div><div id = 'leftInnerSection2' class = 'leftInnerSection' style = 'color: red'>Prerequesites Not Met</div>");
+					$("#right")
+						.append("<input type = 'text' placeholder = 'Search For Courses...'><button type = 'submit'>GO</button><br/><br/><div id = 'rightInnerSection' class = 'rightInnerSection'></div>"
+							+ "<select name = 'courseDropdown'><option>***Choose By Major***</option></select>"
+							+ "<div id = 'rightInnerSection2' class = 'rightInnerSection'></div>");
+						
+					$("select").change(function() {
+						if($("select option:selected").val() == "sd") 
+						{
+							$("#innerSection").load("Checksheets/v1.1/cscSDChecksheet.php");
+							$("#innerSection").animate({ scrollTop: 0 }, "fast"); 
+						}
+						else if($("select option:selected").val() == "Msd") 
+						{
+							$("#innerSection").load("Checksheets/v1.1/cscSDMasterChecksheet.php");
+							$("#innerSection").animate({ scrollTop: 0 }, "fast"); 
+						}
+						else if($("select option:selected").val() == "msd") 
+						{
+							$("#innerSection").load("Checksheets/v1.1/cscSDMinorChecksheet.php");
+							$("#innerSection").animate({ scrollTop: 0 }, "fast"); 
+						}
+						else if($("select option:selected").val() == "it") 
+						{
+							$("#innerSection").load("Checksheets/v1.1/cscITChecksheet.php");
+							$("#innerSection").animate({ scrollTop: 0 }, "fast"); 
+						}
+						else if($("select option:selected").val() == "Mit") 
+						{
+							$("#innerSection").load("Checksheets/v1.1/cscITMasterChecksheet.php");
+							$("#innerSection").animate({ scrollTop: 0 }, "fast"); 
+						}
+						else 
+						{
+							$("#innerSection").load("Checksheets/v1.1/cscITMinorChecksheet.php");
+							$("#innerSection").animate({ scrollTop: 0 }, "fast"); 
+						}
+					}) .change();
+				});
 			});
 		</script>
 	</head>
