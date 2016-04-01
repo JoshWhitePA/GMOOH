d607fa44eac8f913dfffd073da0c705cb9043fb8
Christopher Steckhouse <CSteckhouse6467@students.mc3.edu>
2016-04-01 10:41:30 -0400

    Typo
    It's the littlest things sometimes.

--- Interface/gmoohHome.php
+++ Interface/gmoohHome.php
@@ -7,7 +7,7 @@
 		<script>
 			$(document).ready(function(){
 				$("#master").load("MasterPages/masterPage.html", function() {
-					$("mainSection").append("<div id ='innerSection' class ='innerSection'></div>");
+					$("#mainSection").append("<div id ='innerSection' class ='innerSection'></div>");
 					$("#innerSection").load("Checksheets/v1.1/min/cscITChecksheetSaved.php"); 
 				});
 			});
