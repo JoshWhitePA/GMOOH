7295ed35ace6abef26395c12a288e3659564c9e1
Christopher Steckhouse <CSteckhouse6467@students.mc3.edu>
2016-04-11 11:20:29 -0400

    Update
    Added a proper button and some placeholder text to the table. Further
    updated the stylesheet with some tweaks to width and collapsed borders
    (though I don't know why it doesn't like the border-radius property).

--- Interface/advisor_home.php
+++ Interface/advisor_home.php
@@ -11,14 +11,17 @@ Created for CSC 355WI 020 -->
 			$(document).ready(function(){
 				$("#master").load("MasterPages/masterPage.html", function() {
 					$("#mainSection")
-						.append("<div style='text-align:right'><form action='advisor_home.php' method='post' id='search'> <input type='text'/><input type='submit' value='Search' /> </form></div>"
+						.append("<div style='text-align:right'><form action='advisor_home.php' method='get' id='search'> <input type='text'/><input type='submit' value='Search' /> </form></div>"
+							+ "<form action='' method='post' id='selectAdvisee'>"
 							+ "<table class='adviseeList' id = 'advisorList'>"
-							+ "<tr class='rowSel'><th>Advisee</th><th>Major</th></tr>"
-							+ "<tr class='rowSel' ><td><span class='box'>NAME</span></td><td>MAJOR</td><td>BUTTON</td></tr>"
+							+ "<tr class='rowSel'><th>Advisee</th><th>Major</th><th></th></tr>"
+							+ "<tr class='rowSel' ><td><span class='box'>Jeb Kerman</span></td><td>Science!</td><td><input type='submit' name='SID110' value='View'/></td></tr>"
 							+ "</table>"
+							+ "</form>"
 							);
-					$("advisorList").append("<tr class='rowSel'><td>Advisee2</td><td>Major2</td><td>Button2</td></tr>"
-						);
+						$("advisorList").append("<!-- This is a test -->"
+							+ "<tr class='rowSel'><td>Advisee2</td><td>Major2</td><td>Button2</td></tr>"
+							);
 				});
 			});
 		</script>
