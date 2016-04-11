9f179ce11e8f6ccb8edf356f904d3aa056e93648
ccarr419 <ccarr419@live.kutztown.edu>
2016-04-11 13:57:12 -0400

    Josh's Test Files
    Here are the files Josh wanted to use to test his ideas out on.

---
+++ Interface/joshsTestChecksheet.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Create New Checksheet</title>
+		<link rel = "stylesheet" type = "text/css" href = "Styles/gmoohMasterStyle.css"/>
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.min.css">
+		<script src = "Scripts/jquery-1.12.0.min.js"></script>
+		<script src = "Scripts/joshsTestScript.js"></script>
+		<script>
+			$(window).load(function() {
+				$(".blank").show();
+				$(document).ready(pageLoad(false));
+				$(".blank").delay(500).fadeOut(1000);
+			});	
+		</script>
+	</head>
+	<body id = "behindTheScenes">
+		<div class = 'blank'><div class = "loadingImg"></div><div class = "loadingText">Loading</div></div>
+		<div id = "master" class = "pageBody"></div>
+	</body>
+</html>
