92ee4f1573f421c3dc9e305c32e795261122a251
Christopher Steckhouse <CSteckhouse6467@students.mc3.edu>
2016-04-11 10:27:36 -0400

    Small update
    Still can't get this working.

--- Interface/advisor_home.php
+++ Interface/advisor_home.php
@@ -11,28 +11,19 @@ Created for CSC 355WI 020 -->
 			$(document).ready(function(){
 				$("#master").load("MasterPages/masterPage.html", function() {
 					$("#mainSection")
-						$("#mainSection")
 						.append("<div style='text-align:right'><form action='advisor_home.php' method='post' id='search'> <input type='text'/><input type='submit' value='Search' /> </form></div>"
 							+ "<table class='adviseeList' id = 'advisorList'>"
 							+ "<tr class='rowSel'><th>Advisee</th><th>Major</th></tr>"
 							+ "<tr class='rowSel' ><td><span class='box'>NAME</span></td><td>MAJOR</td><td>BUTTON</td></tr>"
 							+ "</table>"
 							);
-						$("advisorList").append("<!-- This is a test -->"
-							+ "<tr class='rowSel'><td>Advisee2</td><td>Major2</td><td>Button2</td></tr>"
-							);
+					$("advisorList").append("<tr class='rowSel'><td>Advisee2</td><td>Major2</td><td>Button2</td></tr>"
+						);
 				});
 			});
 		</script>
 	</head>
 	<body id = "master">
 	
-	<!-- Notes on some implementation bits:
-			Original concept used a table with each advisee listed.
-				Seemed like it wouldn't work that well in practice (advisor may have quite a few advisees).
-			Dropdown needs to be populated from list of advisees. 
-			Do we actually need a reset button? I left it there because I figured it MIGHT be handy,
-				but I could easily see it being removed.
-			Radio buttons to select action means that the page it's submitted to needs to redirect to the correct action. -->
 	</body>
 </html>
