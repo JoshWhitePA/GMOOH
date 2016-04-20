d1e34be79cee40095235e7b594caeccf6810421f
mpara595 <mpara595@live.kutztown.edu>
2016-04-20 16:10:01 -0400

    checkbox for advisor
    added the adviser checkbox, noticed there was already an isset( "facID") so I just used that name for the checkbox. Should redirect to the adviser home page when the check box is checked

--- Interface/login.php
+++ Interface/login.php
@@ -27,6 +27,10 @@
 						<th>Password &nbsp;</th>
 						<td><input type="password" name="password" id="password" required/></td>
 					</tr>
+					<tr>
+						<th>Advisor? &nbsp;</th>
+						<td><input type = "checkbox" id = "facID" value = "facID" />
+					</tr>
 					<tr height = "10px"></tr>
 					<tr>
 						<th colspan = "2" align = "center">
