d1f1038293442b29b40d78c2a0604b6572818e8a
Christopher Steckhouse <CSteckhouse6467@students.mc3.edu>
2016-04-21 21:29:12 -0400

    Revert "Alright"
    This reverts commit 5f81c34e9f9ce5d50246d25e186b48306322b2e5.

--- Interface/MasterPages/masterPage.php
+++ Interface/MasterPages/masterPage.php
@@ -35,7 +35,7 @@ session_start();
 		</script>
 	</head>
 	<body>	
-		<div id = "header" class = "mainHeader">Welcome <?php echo $name; ?> (<a class = "logout" href = "logout.php">Log out</a>)</div>
+		<div id = "header" class = "mainHeader">Welcome <?php echo json_encode($name); ?> (<a class = "logout" href = "logout.php">Log out</a>)</div>
 		<div class = "newSection"></div>
 		<div id = "headerBar" class = "headerBar">
 			<a href = "gmoohHome.php"><img src = "Images/gmoohLogo.png" class = "logo"/></a>
